c10dc8ab0c0e03e65a6289ed6348adcd71cb9590
Vladimir <vladimir.restivo@gmail.com>
2018-02-20 18:03:08 -0800

    add:		Add unit tests and implementation for 		YahooFinace Stock historical quote to 		List<Entry> and CSV to List<Entry> conversions 		in Util class

--- app/src/main/java/com/example/devbox/stockhawkrewrite/model/Util.java
+++ app/src/main/java/com/example/devbox/stockhawkrewrite/model/Util.java
@@ -1,7 +1,10 @@
 package com.example.devbox.stockhawkrewrite.model;
 
+import android.support.annotation.NonNull;
+
 import com.github.mikephil.charting.data.Entry;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -13,6 +16,12 @@ import yahoofinance.histquotes.HistoricalQuote;
  */
 
 public class Util {
+
+    public static final String REGEX_DATE = "\\d{0,14}";
+    public static final String REGEX_PRICE = "\\d{0,7}\\.\\d{0,7}";
+
+
+
     public static List<IStockDto> convertStockData(Map<String, Stock> stockData) {
         //TODO implement
         return null;
@@ -23,13 +32,47 @@ public class Util {
         return null;
     }
 
-    public static List<Entry> historicalStockQuotesCSVtoEntryList(String csvData) {
-        //TODO implement
+    /**
+     * converts raw historical stock data in CSV format
+     * to List<Entry> usable for the Chart class
+     * @param rawData raw stock price data in CSV format
+     * @return entries in List<Entry> format, readty for the
+     * Chart class consumption
+     */
+    public static List<Entry> historicalStockQuotesCSVtoEntryList(@NonNull String rawData){
+        String[] lineArray;
+        List<Entry> entries = new ArrayList<Entry>();
+        String dateString = null;
+        String priceString = null;
+
+        if(rawData!=null){
+            lineArray = rawData.split("[\n]");
+            for(String valuePair : lineArray){
+                String[] pairSplit = valuePair.split("[,]");
+                if(pairSplit!=null && pairSplit.length == 2
+                        && pairSplit[0] != null && pairSplit[1] != null) {
+                    dateString = pairSplit[0].trim();
+                    priceString = pairSplit[1].trim();
+                    if (dateString.matches(REGEX_DATE) && priceString.matches(REGEX_PRICE)) {
+                        long date = Long.parseLong(dateString);
+                        float price = Float.parseFloat(priceString);
+                        entries.add(new Entry(date, price));
+                    }
+                }
+            }
+            return entries;
+        }
         return null;
     }
 
     public static List<Entry> historicalStockHistoricalQuoteListtoEntryList(List<HistoricalQuote> quotes) {
-        //TODO implement
-        return null;
+        List<Entry> result = new ArrayList<>();
+        for(HistoricalQuote quote : quotes){
+            if(quote.getDate().getTimeInMillis() >0 && quote.getClose()!=null){
+                result.add(new Entry(quote.getDate().getTimeInMillis(), quote.getClose().floatValue()));
+            }
+        }
+
+        return result;
     }
 }
--- app/src/test/java/com/example/devbox/stockhawkrewrite/UtilTest.java
+++ app/src/test/java/com/example/devbox/stockhawkrewrite/UtilTest.java
@@ -9,6 +9,7 @@ import org.junit.Test;
 import org.junit.runners.Parameterized;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
@@ -22,45 +23,162 @@ import yahoofinance.histquotes.HistoricalQuote;
 
 public class UtilTest {
 
-    private String testTicker = "TSLA";
-    private Stock testStock;
+    public static String sTestData = "1454313600000,\n" +
+            "1456819200000, 55.230000\n" +
+            "1459494000000, 49.869999\n" +
+            "1462086000000, 53.000000\n" +
+            "1464764400000, 51.169998\n" +
+            "1467356400000, 56.680000\n" +
+            "1470034800000, 57.459999\n" +
+            "1472713200000, 57.599998\n" +
+            "1475305200000, 59.919998\n" +
+            "1477983600000, 60.259998\n" +
+            "1480579200000, 62.139999\n" +
+            "1483257600000, 64.650002\n" +
+            "1485936000000, 63.980000\n" +
+            "1488355200000, 65.860001\n" +
+            "1491030000000, 68.459999\n" +
+            "1493622000000, 69.839996\n" +
+            "1496300400000, 68.930000\n" +
+            "1498892400000, 72.699997\n" +
+            "1501570800000, 74.769997\n" +
+            "1504249200000, 74.489998\n" +
+            "1506841200000, 83.180000\n" +
+            "1509519600000, 84.169998\n" +
+            "1512115200000, 85.540001\n" +
+            "1514793600000, 95.010002\n" +
+            "1517472000000, 89.610001\n" +
+            "1518076800000, 85.010002\n";
+
+    public static float[] sTestFloatValues = {
+            55.230000F,
+            49.869999F,
+            53.000000F,
+            51.169998F,
+            56.680000F,
+            57.459999F,
+            57.599998F,
+            59.919998F,
+            60.259998F,
+            62.139999F,
+            64.650002F,
+            63.980000F,
+            65.860001F,
+            68.459999F,
+            69.839996F,
+            68.930000F,
+            72.699997F,
+            74.769997F,
+            74.489998F,
+            83.180000F,
+            84.169998F,
+            85.540001F,
+            95.010002F,
+            89.610001F,
+            85.010002F,
+    };
+
+    public static long[] sTestLongVaues = {
+            1456819200000L,
+            1459494000000L,
+            1462086000000L,
+            1464764400000L,
+            1467356400000L,
+            1470034800000L,
+            1472713200000L,
+            1475305200000L,
+            1477983600000L,
+            1480579200000L,
+            1483257600000L,
+            1485936000000L,
+            1488355200000L,
+            1491030000000L,
+            1493622000000L,
+            1496300400000L,
+            1498892400000L,
+            1501570800000L,
+            1504249200000L,
+            1506841200000L,
+            1509519600000L,
+            1512115200000L,
+            1514793600000L,
+            1517472000000L,
+            1518076800000L
+    };
+
+    private String mTestTicker = "IBM";
+    private Stock mTestStock;
     private List<Entry> priceEntryList;
 
-    @Test
-    public void historicalQuoteListToChartEntryListConversion(){
-        givenAValidStockObject();
-        whenConvertingBetweenListTypes();
-        aValidEntryListIsCreated();
 
+    @Test
+    public void parseCSVtoEntryListTest() {
+        String stringCSV = givenCSVDataString();
+        List<Entry> result = covertCSVDataStringToEntryList(stringCSV);
+        retainingOnlyValidValues(result);
     }
 
-    private void givenAValidStockObject() {
+    private String givenCSVDataString() {
+        return sTestData;
+    }
+
+    private List<Entry> covertCSVDataStringToEntryList(String testData) {
+        return Util.historicalStockQuotesCSVtoEntryList(testData);
+    }
+
+
+    private void retainingOnlyValidValues(List<Entry> results) {
+        int counter = 0;
+        for (Entry result : results) {
+            Assert.assertEquals(results.get(counter).getX(), (float) sTestLongVaues[counter]);
+            Assert.assertEquals(results.get(counter).getY(), sTestFloatValues[counter]);
+            counter++;
+        }
+    }
+
+    @Test
+    public void historicalQuoteListToChartEntryListConversion() {
+       Stock testStock = givenAValidStockObject();
+        List<Entry> result = whenConvertingBetweenListTypes(testStock);
+        aValidEntryListIsCreated(testStock, result);
+    }
+
+    private Stock givenAValidStockObject() {
+        Stock testStock = null;
         try {
-            testStock = YahooFinance.get(testTicker);
+            testStock = YahooFinance.get(mTestTicker);
             Assert.assertNotNull(testStock);
         } catch (IOException e) {
             e.printStackTrace();
         }
+        return testStock;
     }
 
 
-    private void whenConvertingBetweenListTypes() {
+    private List<Entry> whenConvertingBetweenListTypes(Stock stock) {
+        List<Entry> result = new ArrayList<>();
         try {
-            priceEntryList = Util.historicalStockHistoricalQuoteListtoEntryList(testStock.getHistory());
-            Assert.assertNotNull(priceEntryList);
+            result = Util.historicalStockHistoricalQuoteListtoEntryList(stock.getHistory());
+            Assert.assertNotNull(result);
+            return result;
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        return result;
     }
 
-    private void aValidEntryListIsCreated(){
+    private void aValidEntryListIsCreated(Stock stock, List<Entry> result) {
         int counter = 0;
         try {
-            for(HistoricalQuote quote : testStock.getHistory()){
-                Assert.assertEquals(quote.getClose(), priceEntryList.get(counter));
-                counter++;
+            List<HistoricalQuote> history = stock.getHistory();
+            for (HistoricalQuote quote : history) {
+                if (quote.getDate().getTimeInMillis() > 0 && quote.getClose() != null) {
+                    Assert.assertEquals(quote.getClose().floatValue(), result.get(counter).getY());
+                    counter++;
+                }
             }
-        } catch (IOException e) {
+        } catch (IOException e){
             e.printStackTrace();
         }
     }
