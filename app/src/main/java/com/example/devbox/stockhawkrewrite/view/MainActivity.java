50a575900e85f7ce419006e0423e8cfaa9405f11
Vladimir <vladimir.restivo@gmail.com>
2018-03-17 18:26:29 -0700

    add:		add basic Stock List UI

--- app/src/main/java/com/example/devbox/stockhawkrewrite/view/MainActivity.java
+++ app/src/main/java/com/example/devbox/stockhawkrewrite/view/MainActivity.java
@@ -1,15 +1,110 @@
 package com.example.devbox.stockhawkrewrite.view;
 
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.devbox.stockhawkrewrite.R;
+import com.example.devbox.stockhawkrewrite.model.StockDto;
+import com.example.devbox.stockhawkrewrite.presenter.IStockListPresenter;
+import com.example.devbox.stockhawkrewrite.presenter.StockListPresenter;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements IStockListView {
+
+    private String LOG_TAG = getClass().getCanonicalName();
+    private IStockListPresenter mPresenter;
+    private RecyclerView mStockRecyclerView;
+    private StockListAdapter mAdapter;
+    private TextInputEditText mEnterStockField;
+    private Button mAddStockButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        mStockRecyclerView = findViewById(R.id.stocks_list);
+        mAdapter = new StockListAdapter();
+        mStockRecyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+        mStockRecyclerView.setAdapter(mAdapter);
+        mEnterStockField = findViewById(R.id.ticker_input_edit_text);
+        //TODO add input filter
+        mAddStockButton = findViewById(R.id.add_stock_button);
+        mAddStockButton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        String ticker = mEnterStockField.getText().toString();
+                        mPresenter.addAStock(ticker);
+                    }
+                }
+        );
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mPresenter = new StockListPresenter(this, getApplicationContext());
+        mPresenter.loadStocks();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.v(LOG_TAG,"_in onStop()");
+        mPresenter.cleanup();
+    }
+
+    @Override
+    protected void onPause() {
+        Log.v(LOG_TAG,"_in onPause()");
+        super.onPause();
+    }
+
+    @Override
+    public void displayError(String errorMessage) {
+        Toast.makeText(getApplicationContext(),
+                errorMessage,
+                Toast.LENGTH_SHORT)
+                .show();
+    }
+
+    @Override
+    public void showStockList() {
+
+    }
+
+    @Override
+    public void showStockDetails() {
+
+    }
+
+    @Override
+    public void forceDataUpdate() {
+
+    }
+
+    @Override
+    public void onStockListLoaded(List<StockDto> stockDtoList) {
+        if(mAdapter!=null){
+            mAdapter.setStockList(stockDtoList);
+            //TODO delete
+            Toast.makeText(this, "newStockList size: " + stockDtoList.size(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void showListIsEmpty() {
+
+    }
+
 }
---
+++ app/src/main/java/com/example/devbox/stockhawkrewrite/view/StockListAdapter.java
@@ -0,0 +1,77 @@
+package com.example.devbox.stockhawkrewrite.view;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.devbox.stockhawkrewrite.R;
+import com.example.devbox.stockhawkrewrite.model.StockDto;
+
+import java.util.List;
+
+/**
+ * ListView Adapter Stub
+ */
+
+public class StockListAdapter extends RecyclerView.Adapter<StockListAdapter.StockListItemViewHolder> {
+
+    private List<StockDto> mStockList;
+
+
+
+    @Override
+    public StockListItemViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+       View viewHolderRoot = LayoutInflater
+               .from(parent.getContext())
+               .inflate(R.layout.stock_list_item, parent, false);
+
+       return new StockListItemViewHolder(viewHolderRoot);
+    }
+
+    @Override
+    public void onBindViewHolder(StockListItemViewHolder holder, int position) {
+        if(mStockList!=null && position < mStockList.size()){
+            holder.mmTicker.setText(mStockList.get(position).getTicker());
+            holder.mmPrice.setText(String.valueOf(mStockList.get(position).getAsk()));
+            holder.mmPriceChange.setText(String.valueOf(mStockList.get(position).getChangePercent()));
+        }
+    }
+
+    public void setStockList(List<StockDto> newList){
+        mStockList = newList;
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public int getItemCount() {
+        if(mStockList!=null){
+            return mStockList.size();
+        }
+        return 0;
+    }
+
+    public class StockListItemViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        //TODO
+        View mmRootView;
+        public TextView mmTicker;
+        public TextView mmPrice;
+        public TextView mmPriceChange;
+
+        public StockListItemViewHolder(View itemView) {
+            super(itemView);
+            mmRootView = itemView;
+            mmTicker = mmRootView.findViewById(R.id.stock_list_item_ticker);
+            mmPrice = mmRootView.findViewById(R.id.stock_list_item_price);
+            mmPriceChange = mmRootView.findViewById(R.id.stock_list_item_price_change);
+        }
+
+        @Override
+        public void onClick(View v) {
+            //TODO implement
+        }
+    }
+
+}
